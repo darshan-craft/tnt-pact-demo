7fcc50f52ba54d8d4dee737cd612588d09dbeff9
sharm40 <g.sharma@elsevier.com>
2020-04-24 15:28:43 -0400

    added pact test class

--- pactdemo-javaclient/src/main/java/org/example/client/PactDemoClient.java
+++ pactdemo-javaclient/src/main/java/org/example/client/PactDemoClient.java
@@ -5,20 +5,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class PactDemoClient {
 
     private final String url;
-    @Autowired
+  
     private RestOperations restOps;
 
     @Autowired
     public PactDemoClient(@Value("${pactdemo.service.url}") final String url) {
+
         this.url = url;
     }
 
     public MessageDto getWelcomeMsg(final String name) {
+        restOps = new RestTemplate() ;
         MessageDto dto = restOps.getForObject(url, MessageDto.class, name);
         return dto;
     }
---
+++ pactdemo-javaclient/src/test/java/org/example/client/PactDemoClientTest.java
@@ -0,0 +1,59 @@
+package org.example.client;
+
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRuleMk2;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.RequestResponsePact;
+import com.elsevier.submissions.dto.MessageDto;
+import io.pactfoundation.consumer.dsl.LambdaDsl;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        properties = "pactdemo-service.base-url:http://localhost:8080",
+        classes = PactDemoClient.class)
+public class PactDemoClientTest {
+
+    @Rule
+    public PactProviderRuleMk2 provider = new PactProviderRuleMk2("pactdemo-service", null,
+            8080, this);
+
+    @Autowired
+    private PactDemoClient pactDemoClient;
+
+    @PactVerification(fragment = "pactWelcomeMessageExists")
+    @Test
+    public void welcomeMessageExists() {
+        MessageDto dto = pactDemoClient.getWelcomeMsg("TheJungle");
+        Assert.assertEquals(dto.getMessage(), "Welcome to TheJungle !");
+    }
+
+    //JSONObject obj = new JSONObject().put("message", "Welcome to TheJungle !");
+
+
+    @Pact(consumer = "messaging-app")
+    public RequestResponsePact pactWelcomeMessageExists(PactDslWithProvider builder) {
+        return builder.given("Message exists")
+                .uponReceiving("A request to /welcome/TheJungle")
+                .path("/welcome/TheJungle")
+                .method("GET")
+                .willRespondWith()
+                .status(200)
+                .body(LambdaDsl.newJsonBody((o) -> o
+                        .stringType("message", "Welcome to TheJungle !"))
+                .build())
+                .toPact();
+    }
+
+
+
+
+}
